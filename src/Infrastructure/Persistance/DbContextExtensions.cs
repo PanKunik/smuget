158035b64104962df3457884092c207d28cdcca0
Patryk Kunicki <30160044+PanKunik@users.noreply.github.com>
2023-09-12 12:29:16 +0200

    #30 Feature - Improve application configuration (#34)
    * Extracting connection string to appsettings
    
    * Readme update
    
    * Restarting API until DB is ready

--- src/Infrastructure/Persistance/DbContextExtensions.cs
+++ src/Infrastructure/Persistance/DbContextExtensions.cs
@@ -1,14 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.Persistance;
 
 internal static class DbContextExtensions
 {
-    internal static IServiceCollection AddDbContext(this IServiceCollection services)
+    internal static IServiceCollection AddDbContext(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<SmugetDbContext>(
-            c => c.UseNpgsql("Host=db;Database=postgres;Username=postgres;Password=example")
+            c => c.UseNpgsql(configuration.GetConnectionString("smuget-db"))
         );
 
         services.AddHostedService<DatabaseInitializer>();
--- src/WebAPI/Program.cs
+++ src/WebAPI/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddApplication()
-        .AddInfrastructure();
+        .AddInfrastructure(builder.Configuration);
 
     builder.Services.AddHTTPLogging();
 
