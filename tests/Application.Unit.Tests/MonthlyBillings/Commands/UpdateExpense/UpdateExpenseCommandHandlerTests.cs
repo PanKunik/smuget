f8de3547836186fdaca45cf18899bb4cd7efc62c
PanKunik <patryk.kunicki@pm.me>
2023-10-02 12:48:39 +0200

    Adding expense updating in application layer

---
+++ tests/Application.Unit.Tests/MonthlyBillings/Commands/UpdateExpense/UpdateExpenseCommandHandlerTests.cs
@@ -0,0 +1,68 @@
+using Application.Exceptions;
+using Application.MonthlyBillings.Commands.UpdateExpense;
+using Application.Unit.Tests.MonthlyBillings.Commands.TestUtilities;
+using Application.Unit.Tests.TestUtilities;
+using Application.Unit.Tests.TestUtilities.Constants;
+using Domain.MonthlyBillings;
+using Domain.Repositories;
+
+namespace Application.Unit.Tests.MonthlyBillings.Commands.UpdateExpense;
+
+public sealed class UpdateExpenseCommandHandlerTests
+{
+    private readonly IMonthlyBillingRepository _repository;
+    private readonly UpdateExpenseCommandHandler _handler;
+
+    public UpdateExpenseCommandHandlerTests()
+    {
+        _repository = Substitute.For<IMonthlyBillingRepository>();
+        _handler = new UpdateExpenseCommandHandler(_repository);
+    }
+
+    [Fact]
+    public async Task HandleAsync_WhenCalled_ShouldCallGetByIdOnRepositoryOnce()
+    {
+        // Arrange
+        var command = UpdateExpenseCommandUtilities.CreateCommand();
+
+        _repository
+            .GetById(new(Constants.MonthlyBilling.Id))
+            .Returns(MonthlyBillingUtilities.CreateMonthlyBilling(
+                plans: new List<Plan>()
+                {
+                    PlansUtilities.CreatePlan(
+                        new List<Expense>()
+                        {
+                            ExpenseUtilities.CreateExpense()
+                        }
+                    )
+                }
+            ));
+
+        // Act
+        await _handler.HandleAsync(
+            command,
+            default
+        );
+
+        // Act & Assert
+        await _repository
+            .Received(1)
+            .GetById(new(command.MonthlyBillingId));
+    }
+
+    [Fact]
+    public async Task HandleAsync_WhenMonthlyBillingNotFound_ShouldThrowMonthlyBillingNotFound()
+    {
+        // Arrange
+        var command = UpdateExpenseCommandUtilities.CreateCommand();
+
+        var updateIncome = async () => await _handler.HandleAsync(
+            command,
+            default
+        );
+
+        // Act & Assert
+        await Assert.ThrowsAsync<MonthlyBillingNotFoundException>(updateIncome);
+    }
+}
